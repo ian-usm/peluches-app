09afc95c8a358bd8f4b0891bcdcf780d87779174
ian-usm <135771194+ian-usm@users.noreply.github.com>
2026-03-02 19:13:30 -0300

    initial Next.js setup

---
+++ src/app/creador/page.tsx
@@ -0,0 +1,9 @@
+export default function CreadorPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h2 className="text-2xl font-semibold">
+        Aquí irá el personalizador 🧸
+      </h2>
+    </main>
+  );
+}
