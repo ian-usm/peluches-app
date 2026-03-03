9c6a87886aaa21f82596b5011b21c333eac064f0
ian-usm <135771194+ian-usm@users.noreply.github.com>
2026-03-03 15:22:28 -0300

    inicio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center">
       <h1 className="text-3xl font-bold">
-        Peluches Kawaii Oscuros 🖤
+        🖤
       </h1>
     </main>
   );
